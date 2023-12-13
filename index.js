199fc30338792faa583078954ee456f0e2fbeba2
Chirag sadhwani <138006300+chiragop1234@users.noreply.github.com>
2023-12-13 15:25:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const Discord = require('discord.js');
 const { Intents } = require('discord.js');
 const client = new Discord.Client({
   intents: [
-    Intents.FLAGS.GUILDS,                // Required for server-related events
-    Intents.FLAGS.GUILD_MESSAGES,        // Required for message-related events
-    Intents.FLAGS.GUILD_MEMBERS,         // Required if you need member events
-    // Add more intents as needed
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_PRESENCES,        // Add this line for presence updates
+    Intents.FLAGS.GUILD_VOICE_STATES,     // Add this line if interacting with voice channels
+    Intents.FLAGS.MESSAGE_CONTENT,        // Add this line for message content
+    Intents.FLAGS.MESSAGE_REACTIONS,      // Add this line for tracking reactions
+    Intents.FLAGS.MESSAGE_TYPING,         // Add this line for tracking typing events
+    Intents.FLAGS.DIRECT_MESSAGES         // Add this line for direct messages
   ],
 });
 const { handleMoneyCommands } = require('./money');
