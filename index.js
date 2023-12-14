d3890aed9aac021a2b904bd017fb862fc31cc6e0
Chirag sadhwani <138006300+chiragop1234@users.noreply.github.com>
2023-12-14 18:12:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Retrieve the bot token from the environment variable
+const token = process.env.BOT_TOKEN;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
-  if (message.content.toLowerCase() === 'hi') {
-    message.reply('Hello!');
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
 });
 
-client.login(process.env.TOKEN);
+// Add more event handlers and commands as needed
+
+// Login to Discord with the bot token
+client.login(token);
